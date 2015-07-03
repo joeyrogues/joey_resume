c52d8466d9ad2ec1c94ffc8da625bed9d2643997
Joey Rogues <rogues.jonathan.168@gmail.com>
2015-07-03 17:14:15 +0200

    Added education

--- app/education/education.js
+++ app/education/education.js
@@ -9,6 +9,46 @@ angular.module('joey_resume.education', ['ngRoute'])
         });
     }])
 
-    .controller('EducationCtrl', ['$scope', function ($scope) {
+    .controller('EducationCtrl', ['$scope', 'Education', function ($scope, Education) {
+        $scope.educations = [
+            new Education({
+                school:    'ENSIIE',
+                city:      'Evry',
+                startDate: '2011-09-05',
+                endDate:   '2015-02-25',
+                diploma:   'Engineer degree',
+                summary:   'abc'
+            }),
+            new Education({
+                school:    'Institute of Technology',
+                city:      'Fontainebleau',
+                startDate: '2009-14-14',
+                endDate:   '2011-06-24',
+                diploma:   'Diploma of Technology in Computing',
+                summary:   'def'
+            }),
+            new Education({
+                school:    'Boulloche Highschool',
+                city:      'Livry Gargan',
+                startDate: '2008-06-20',
+                endDate:   '2004-09-13',
+                diploma:   'Baccalauréat',
+                summary:   'ghi'
+            })
+        ];
+    }])
 
+    .factory('Education', [function() {
+        var Education = function(params) {
+            this.school = params.school;
+            this.city = params.city;
+
+            this.startDate = params.startDate;
+            this.endDate = params.endDate;
+
+            this.diploma = params.diploma;
+            this.summary = params.summary;
+        };
+
+        return Education;
     }]);
